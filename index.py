d9ae54cb5a3462ff8800e294b8f9b7f3d5c9e391
Abdulla Bashiroglu <bashiroglu.abdulla@gmail.com>
2025-10-21 20:17:11 +0400

    add Hurricane Analysis project

--- index.py
+++ index.py
@@ -1,2 +1,4 @@
-a = 9
-print(a)
+oscars = {"Best Picture": "Moonlight", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+
+for element in oscars:
+  print(element)
